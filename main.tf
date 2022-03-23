426d0266bcea8a6202edf8557636b7134933d8f2
Thomas Zimny <tomzimny@gmail.com>
2022-03-23 10:33:26 -0400

    23 March 2022 VPC Constructed

--- main.tf
+++ main.tf
@@ -20,6 +20,23 @@ provider "aws" {
     
 }
 
+resource "aws_vpc" "MC-EKS-VPC" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MC-EKS-VPC"
+  }
+}
+
+resource "aws_subnet" "Subnet-1" {
+  vpc_id     = aws_vpc.MC-EKS-VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "MC-Subnet"
+  }
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.11.0"
